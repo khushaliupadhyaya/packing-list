440ee027bf9f62dcd66ac118703d22c80131d505
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-05-17 18:19:02 -0400

    Pass object into array instead of just the name

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 //day 2 and day 3
 const form = document.querySelector('#itemForm')
 let itemArray = new Array
-//let item
+//let itemName
 
 
-function makeList(itemName) {
+function makeList(thing) {
     const list = document.createElement('ul')
-
+    
     //const item = document.createElement('li')
     item = document.createElement('li')
-    item.textContent = itemName
+    // item.textContent = itemName
+    item.textContent = thing['name']
 
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = 'Delete'
     deleteButton.addEventListener('click', deleteItem)
+
+    //deleteButton.tagName = itemName
     //deleteButton.addEventListener('click', deleteFromArray(item))
     //deleteButton.setAttribute('type', 'reset')
     // deleteButton.addEventListener('onclick', function(e) {
@@ -29,11 +32,14 @@ function deleteItem(ev) {
     //var element = `${itemName}`
     ev.preventDefault()
     const btn = ev.target
+    //btn.name = itemName
     //gets the button but cannot access to the name/li that the button is in. Unable to access the actual element NAME
     btn.parentNode.remove()
-    itemArray.splice(itemArray.indexOf(btn), 1)
+    //itemArray.splice(itemArray.indexOf(itemArray[`${itemName}`], 1))
+     itemArray.splice(itemArray.indexOf(btn), 1)
+    //itemArray.splice(itemArray.indexOf(ev.path[1].childNodes[0].textContent), 1)
     // const l = btn.name;
-    debugger
+    //debugger
 }
 
 // function deleteFromArray(item) {
@@ -53,7 +59,8 @@ const handleSubmit = function(ev) {
     
     const items = document.querySelector('#items')
 
-    items.appendChild(makeList(itemName))
+    // items.appendChild(makeList(itemName))
+    items.appendChild(makeList(thing))
 
     form.reset()
     form.itemName.focus()
